cc7882a97c240d83464e04930821be513bb2659c
Andrey Lutogin <lutogin@trueconf.com>
2019-05-11 15:01:26 +0300

    Fix db

--- app/settings.py
+++ app/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dj_girls_db',
-        'USER': 'django_db_admin',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

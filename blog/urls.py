3bc9c0669f2d27211b419662cd2f30ca6faccbcf
Andrey Lutogin <lutogin@trueconf.com>
2019-05-12 15:18:08 +0300

    Add new post form

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    re_path(r'^add-post', views.add_post, name='add_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.http import HttpResponse
 from .models import Post
+from .forms.forms import PostForm
 
 
 def post_list(req):
     return render(req, 'blog/post_list.twig', context={
-        'posts': Post.objects.all().order_by('published_date'), 'title': 'Blog - главная.'
+        'posts': Post.objects.all().order_by('published_date'),
+        'title': 'Blog - главная.'
     })
+
+
+def add_post(req):
+    if req.method == 'GET':
+        return render(req, 'blog/add-post.twig', {
+            'form': PostForm()
+        })
+    else:
+        form = PostForm(req.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = req.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+        else:
+            return redirect('add_post')

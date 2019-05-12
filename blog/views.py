1c20970737211094165a3588b6a62a8a522adc91
Andrey Lutogin <lutogin@trueconf.com>
2019-05-12 13:34:06 +0300

    Add render template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
 from .models import Post
 
 
 def post_list(req):
-    return render(req, 'blog/post_list.twig', context={'posts': Post.objects.all()})
+    return render(req, 'blog/post_list.twig', context={
+        'posts': Post.objects.all().order_by('published_date'), 'title': 'Blog - главная.'
+    })
